0abe39bf13c65bf8216310afead509f2b2d41a5d
eng-ts <eng-ts@users.noreply.github.com>
2024-05-23 09:44:26 +0000

    deploy: d0208f5d3a8de303eac6ff285b94f0487164a22f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,132 +2,132 @@ var store = [{
         "title": "Maison dans un champs",
         "excerpt":"Photos © Gaelle Le Boulicaut Prix « Coup de coeur Salon Maison Bois » 2006 Site: Auvilliers / Haute-Normandie Surface: 180 m2 Matériaux: Plot béton, sapin du Nord, bardeaux de cèdre rouge, sapin du Nord brûlé Isolation: Laine de roche Chauffage: Poêle de masse C’est ici que l’expérience commence. Jean-Baptiste...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-dans-un-champs/",
+        "url": "/maison-dans-un-champs/",
         "teaser": null
       },{
         "title": "Maison dans le Pré",
         "excerpt":"Photos © arba- 1er Prix « Salon Maison Bois » 2008 Lauréat des « Lauriers de la construction bois » 2009 Site: Épinac / Saône et Loire Surface: 90 m2 Budget: 125 000€ TTC Matériaux: Plot béton, pin douglas, zinc à joint debout, polycarbonate 32mm Isolation: Laine de bois Chauffage:...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-dans-le-pre/",
+        "url": "/maison-dans-le-pre/",
         "teaser": null
       },{
         "title": "Maison Traversée",
         "excerpt":"Photos © arba- Mention « Salon Maison Bois » 2008 Lauréat des « Lauriers de la construction bois » 2009 Site: Rouen / Haute-Normandie Surface: 110 m2 Budget: 140 000€ TTC Matériaux: Plot béton, sapin du Nord, zinc à joints debout, pin douglas, sapin du nord brûlé Isolation: Laine de...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-traversee/",
+        "url": "/maison-traversee/",
         "teaser": null
       },{
         "title": "Maison dans la Forêt",
         "excerpt":"Site: Méré / Yvelines Surface: 200 m2 Budget: 315 000€ TTC Matériaux: Plot béton, sapin du nord, tuiles de terre cuite, bardage mélèze Isolation: Laine minérale Chauffage: Pompe à chaleur et planchers chauffants Pour cette maison, située dans une clairière de la forêt de Rambouillet, l’objectif était de retrouver la...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-dans-la-foret/",
+        "url": "/maison-dans-la-foret/",
         "teaser": null
       },{
         "title": "Maison dans le Jardin",
         "excerpt":"Photos © Hervé Abbadie Lauréat des « 24 heures de l’architecture » 2012 Site: Veneux-Les-Sablons / Ile-de-France Surface: 130 m2 Budget: 250 000€ TTC Matériaux: Plot béton, sapin du nord, tasseaux de mélèze, verre clair et sérigraphié Isolation: Laine de bois Chauffage: Poêle à bois La première fois que nous...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-dans-le-jardin/",
+        "url": "/maison-dans-le-jardin/",
         "teaser": null
       },{
         "title": "Maison dans la Prairie",
         "excerpt":"Photos © arba- 2ème prix National de la Construction Bois 2015​ Lauréat du ​prix « Bois Construction Environnement de Basse-Normandie » 201​​​​​5​ Site: Lisieux / Basse-Normandie Surface: 78 m2 Budget: 136 000€ TTC Matériaux: Plot béton, sapin du nord, bac acier, vêture et menuiseries mélèze Isolation: Laine de bois Chauffage:...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-dans-la-prairie/",
+        "url": "/maison-dans-la-prairie/",
         "teaser": null
       },{
         "title": "La Halle",
         "excerpt":"Photos 1,2,4 © M.Flaux / 3,5,6,7,8,9,10 © F.Le Guen Prix Habitat Durable Archinovo 2015 décerné par Fleur Pellerin Site: Rouen / Normandie Surface: 392 m2 Budget: 360 000€ TTC Matériaux: Sous-sol béton, sapin du nord, menuiserie et vêture mélèze, ardoise, brique Isolation: Laine de bois Chauffage: Poêle à bois Cette...","categories": [],
         "tags": [],
-        "url": "/arba.pro/la-halle/",
+        "url": "/la-halle/",
         "teaser": null
       },{
         "title": "Maison Cosse",
         "excerpt":"Photos 1,2,3,4 © Hervé Abbadie / 5,6 © arba- Site: Montreuil / Ile-de-France Surface: 176 m2 Budget: 200 000€ TTC Matériaux: Dalle béton sur VS, sapin du nord, tasseaux de mélèze, double vitrage clair Isolation: Laine de bois Chauffage: Poêle à bois Cette maison a été dessiné pour un chanteur...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-cosse/",
+        "url": "/maison-cosse/",
         "teaser": null
       },{
         "title": "L’Onde",
         "excerpt":"Photos 1,2,3,5,7,8,9,11,12,13,14,15,© P.Gourvennec- / 4,6,10 © arba- Site: Murat / Cantal Surface: 75 m2 Budget: 205 000€ TTC Matériaux: Dalle béton brut, sapin du nord, zinc prépatiné noir à joint debout, châssis et vêture mélèze Chauffage: Poêle à bois Agencement: Contreplaqué de bouleau et frêne massif local (égoïne). En écho...","categories": [],
         "tags": [],
-        "url": "/arba.pro/l-onde/",
+        "url": "/l-onde/",
         "teaser": null
       },{
         "title": "Petite Maison",
         "excerpt":"Photos © 1,2,7,8,9,11,12,13 © P.Gourvennec / 3,4,5,6,10,14© arba- Architecte associé P.Gourvennec Site: Longueil / Haute-Normandie Surface: 20 m2 Budget: 98 000€ Matériaux: Plots béton, sapin du nord, tôle ondulée et polycarbonate, châssis et vêture mélèze Chauffage: Poêle à bois Il s’agit cette fois de construire un habitat minimal pour profiter...","categories": [],
         "tags": [],
-        "url": "/arba.pro/petite-maison/",
+        "url": "/petite-maison/",
         "teaser": null
       },{
         "title": "Maison filtre",
         "excerpt":"Photos © arba- Site: Chevreuse / Ile-de-France Surface: 110 m2 Budget: 180 000€ TTC Matériaux: Longrines béton, sapin du Nord, vêture mélèze, zinc pré-patiné Isolation: Laine de bois Chauffage: Poêle à bûche Cette maison implantée en cœur de forêt est destinée à une famille pour qui passer du temps dehors...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-filtre/",
+        "url": "/maison-filtre/",
         "teaser": null
       },{
         "title": "Maison près du Sapin",
         "excerpt":"Photos © F.Le Guen / agencement égoïne Site: Loisieux / Savoie Surface: 40 m2 Budget: 96 000€ TTC Matériaux: Longrines, sapin du nord, bardeaux de cèdre rouge, menuiserie mélèze, OSB, 3 plis sapin Isolation: Laine de bois Chauffage: Poêle à bois Ce projet, dans un petit village de Savoie, a...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-pres-du-sapin/",
+        "url": "/maison-pres-du-sapin/",
         "teaser": null
       },{
         "title": "Maison sur la Montagne",
         "excerpt":"Photos © arba- Site: Poisy / Haute-Savoie Surface: 120 m2 Budget: 200 000€ TTC Matériaux: Plot béton, sapin du Nord, HEA métal, tuiles de terre cuite, menuiserie mélèze, polyca. 32mm Isolation: Laine de bois Chauffage: Poêle à bois Lorsqu’ils sont venus nous consulter, nos clients étaient désespérés car un précédant...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-sur-la-montagne/",
+        "url": "/maison-sur-la-montagne/",
         "teaser": null
       },{
         "title": "La Grange",
         "excerpt":"Photos © Kat&lt;/i&gt; / Partenaire associé M.Bouvier Site: Grosrouvre / Ile-de-France Surface: 228 m2 Budget: 382 000€ TTC Matériaux: Dalle béton, pin du nord, tuiles de terre cuite, bardage mélèze, béton ciré, CP Okoumé Isolation: Laine minérale Chauffage: Planchers chauffants sur géothermie La réglementation laissait peu de liberté pour la...","categories": [],
         "tags": [],
-        "url": "/arba.pro/la-grange/",
+        "url": "/la-grange/",
         "teaser": null
       },{
         "title": "Maison Relais",
         "excerpt":"Photos © arba- Site: Gaillon / Haute-Normandie Surface: 175 m2 Budget: 304 000€ TTC Matériaux: Longrines béton, pin du nord, zinc à joint debout, vêture et menuiserie mélèze Isolation: Laine de bois Chauffage: Poêle à bois et puits canadien Le bois est un matériau ayant une faible inertie. Ce manque...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-relais/",
+        "url": "/maison-relais/",
         "teaser": null
       },{
         "title": "Maison Tube",
         "excerpt":"Photos © arba- Site: Saint-Chéron / Ile-de-France Surface: 130 m2 Budget: 235 000€ TTC Matériaux: Plot béton, sapin du nord, toiture végétalisé, chape anhydrite Isolation: Laine de bois Chauffage: Pompe à chaleur et planchers chauffants C’est la configuration du site qui nous a mené à cette forme tubulaire. La maison...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-tube/",
+        "url": "/maison-tube/",
         "teaser": null
       },{
         "title": "La Lanterne",
         "excerpt":"Photos © Daniel Moulinet Site: Les Lilas / Ile-de-France Surface: 45 m2 Budget: 100 000€ TTC Matériaux: Dalle béton, sapin du nord, vêture et menuiseries mélèze, béton poli Isolation: Laine de bois Chauffage: Poêle à bois Implantée au fond du jardin, cette petite construction permet de soulager une maison principale...","categories": [],
         "tags": [],
-        "url": "/arba.pro/la-lanterne/",
+        "url": "/la-lanterne/",
         "teaser": null
       },{
         "title": "Entre les murs",
         "excerpt":"Photos 1,2,5,6 © arba- / 3,4,7,8,9,10,11,12,13,14,15,16,17 © D.Moulinet 1er Prix National de la Construction Bois 2018 Lauréat des Trophées Bois Île-de-France 2018 Site: Thomery / Seine-et-Marne Surface: 108 m2 Budget: 266 000€ TTC Matériaux: Longrines béton, sapin du nord, châssis aluminium, vêture CP et goudron de pin, dalles MFP Chauffage:...","categories": [],
         "tags": [],
-        "url": "/arba.pro/entre-les-murs/",
+        "url": "/entre-les-murs/",
         "teaser": null
       },{
         "title": "Maison de ville",
         "excerpt":"Photos © arba- Site: Chatou / Ile de France Surface: 115 m2 Budget: 300 000€ TTC (livré clef en main, y compris clôtures et jardins) Matériaux: Béton cellulaire, tuiles mécaniques, parquets chêne et agencements médium Isolation: Laine minérale Chauffage: poêle à granule Cette maison construite dans le site classé de...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-de-ville/",
+        "url": "/maison-de-ville/",
         "teaser": null
       },{
         "title": "Le Pavillon",
         "excerpt":"Photos © arba- Site: Flexanville / Yvelines Surface: 130 m2 Budget: 150 000€ TTC Matériaux: Plot béton, sapin du Nord, tuiles de terre cuite, panneaux OSB Isolation: Laine de bois Chauffage: 2 poêles a bois Pour sa maison, ce couple de musiciens demandait beaucoup de lumière, de la transparence, et...","categories": [],
         "tags": [],
-        "url": "/arba.pro/le-pavillon/",
+        "url": "/le-pavillon/",
         "teaser": null
       },{
         "title": "Maison écran",
         "excerpt":"Photos © arba- Architecte associés P.Gourvennec / Archi.Tech Site: Alfortville / Ile-de-France Surface: 50 m2 Budget: 100 000€ Nous avons apprécié concevoir cette extension pour des clients demandeur d’élégance et de sobriété. L’existant était agréable, en grande partie grâce à une série de roseaux qui filtraient la lumière et transmettaient...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-ecran/",
+        "url": "/maison-ecran/",
         "teaser": null
       },{
         "title": "Maison Corse",
         "excerpt":"Photos © arba- Architecte associé JL.Baldacci Site: Morsiglia / Haute Corse Surface: 17 m2 Budget: 96 000€ TTC Matériaux: Longrines béton, pin Laricio, lauze, châssis et vêture mélèze Chauffage: Poêle à bois Pour profiter du cap Corse sans dénaturer ce magnifique paysage, cette petite intendance aux allures de cabane de...","categories": [],
         "tags": [],
-        "url": "/arba.pro/maison-corse/",
+        "url": "/maison-corse/",
         "teaser": null
       }]
